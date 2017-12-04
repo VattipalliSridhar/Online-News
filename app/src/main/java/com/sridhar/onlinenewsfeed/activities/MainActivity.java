bab52f1c75d0f99af729eeab525fd5201c4bc66e
2136 <vattipallisridhar4u@gmail.com>
2017-12-04 11:35:11 +0530

    initial_commit

--- app/src/main/java/com/sridhar/onlinenewsfeed/activities/MainActivity.java
+++ app/src/main/java/com/sridhar/onlinenewsfeed/activities/MainActivity.java
@@ -5,10 +5,17 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -42,35 +49,147 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity
 {
     private RequestQueue requestQueue;
-    private String url = "https://sites.google.com/site/myapps4748/android/abc.json";
+    private String url = null;
     private ProgressDialog pDialog;
     private List<Newsfeeds> newsList = new ArrayList<Newsfeeds>();
     private ListView listView;
     private CustomListAdapter adapter;
     private Context context;
     ImageRequest imageRequest;
+
+    private JsonArrayRequest jsonArrayRequest;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        initNavigationDrawer();
+
+
         requestQueue = VolleySingleton.getVolleySingletonInstance().getRequestQueue();
-
         listView=(ListView)findViewById(R.id.list);
-
-
         context=this;
 
-
-
-        JsonArrayRequest jsonArrayRequest = getJsonArrayRequest();
-
+        url="https://sites.google.com/site/myapps4748/android/abc.json";
+        jsonArrayRequest = getJsonArrayRequest();
         requestQueue.add(jsonArrayRequest);
 
 
 
 
+    }
 
+    public void initNavigationDrawer() {
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                int id = menuItem.getItemId();
+
+                switch (id){
+                    case R.id.all_news:
+
+                        newsList.clear();
+                        url="https://sites.google.com/site/myapps4748/android/abc.json";
+                        jsonArrayRequest = getJsonArrayRequest();
+                        requestQueue.add(jsonArrayRequest);
+
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.world_news:
+
+
+                        newsList.clear();
+                        url="https://sites.google.com/site/onlinenews008/onlinenews/world.json";
+                        jsonArrayRequest = getJsonArrayRequest();
+                        requestQueue.add(jsonArrayRequest);
+
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.national_news:
+
+                        newsList.clear();
+                        url="https://sites.google.com/site/onlinenews008/onlinenews/national.json";
+                        jsonArrayRequest = getJsonArrayRequest();
+                        requestQueue.add(jsonArrayRequest);
+
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.ap_news:
+
+                        newsList.clear();
+                        url="https://sites.google.com/site/onlinenews008/onlinenews/ap_new.json";
+                        jsonArrayRequest = getJsonArrayRequest();
+                        requestQueue.add(jsonArrayRequest);
+
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.tg_news:
+
+                        newsList.clear();
+                        url="https://sites.google.com/site/onlinenews008/onlinenews/tg_news.json";
+                        jsonArrayRequest = getJsonArrayRequest();
+                        requestQueue.add(jsonArrayRequest);
+
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.sports_news:
+
+                        newsList.clear();
+                        url="https://sites.google.com/site/onlinenews008/onlinenews/sports_news.json";
+                        jsonArrayRequest = getJsonArrayRequest();
+                        requestQueue.add(jsonArrayRequest);
+
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.cinema_news:
+
+                        newsList.clear();
+                        url="https://sites.google.com/site/onlinenews008/onlinenews/cinema_news.json";
+                        jsonArrayRequest = getJsonArrayRequest();
+                        requestQueue.add(jsonArrayRequest);
+
+                        drawerLayout.closeDrawers();
+                        break;
+                    case R.id.special_quo:
+
+                        newsList.clear();
+                        url="https://sites.google.com/site/onlinenews008/onlinenews/special_quo.json";
+                        jsonArrayRequest = getJsonArrayRequest();
+                        requestQueue.add(jsonArrayRequest);
+
+                        drawerLayout.closeDrawers();
+                        break;
+                }
+                return true;
+            }
+        });
+        View header = navigationView.getHeaderView(0);
+        TextView tv_email = (TextView)header.findViewById(R.id.app_tv);
+        tv_email.setText(""+getResources().getString(R.string.app_name));
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close){
+
+            @Override
+            public void onDrawerClosed(View v){
+                super.onDrawerClosed(v);
+            }
+
+            @Override
+            public void onDrawerOpened(View v) {
+                super.onDrawerOpened(v);
+            }
+        };
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
     }
 
     private JsonArrayRequest getJsonArrayRequest() {
